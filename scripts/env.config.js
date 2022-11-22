f203056f03c8b6cd67164145165881fab91bdfb4
keep-promise <64321089+keep-promise@users.noreply.github.com>
2022-11-22 19:15:06 +0800

    Update env.config.js

--- scripts/env.config.js
+++ scripts/env.config.js
@@ -1,1 +1,16 @@
+const fs = require('fs');
+const path = require('path');
 
+const env = process.env;
+
+const prod = env.NODE_ENV === 'production';
+const appPath = fs.realpathSync(process.cwd());
+module.exports = {
+  env,
+  prod,
+  appPath,
+  dProtocol: 'https',
+  dHost: env.HOST || 'localhost',
+  dPort: env.PORT != null ? Number(env.PORT) : 8080,
+  demoPath: path.resolve(appPath, './demo')
+};
